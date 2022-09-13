2f893b1708ef83814c253cfc3101490f5272e0e7
Ilan Geller <gellerilan@gmail.com>
2022-09-13 11:20:13 +0300

    more fun CLT-86

--- contracts/FeeDistribution.sol
+++ contracts/FeeDistribution.sol
@@ -14,6 +14,7 @@ contract FeeDistribution is Ownable, ReentrancyGuard {
 
     IERC20 public immutable feesToken;
     IInsurancePool public immutable pool;
+    address public immutable coverMgmtAddress;
 
     uint256 private constant ACC_TOKEN_PRECISION = 1e18;
     uint256 private constant feePerShareRate = 1; // development
@@ -27,14 +28,19 @@ contract FeeDistribution is Ownable, ReentrancyGuard {
     // User address => shareInPool to be claimed // development
     mapping(address => uint256) public shareInPool; // development
 
-    constructor(address _feesToken, address _pool) {
+    constructor(
+        address _feesToken,
+        address _pool,
+        address _coverMgmtAddress
+    ) {
         feesToken = IERC20(_feesToken);
         pool = IInsurancePool(_feesToken);
+        coverMgmtAddress = _coverMgmtAddress;
     }
 
     function claim() external nonReentrant {
         // here we need to check the user share in the pool
-        uint256 uShareInPool = pool.ShareInPool();; //pool.ShareInPool(msg.sender);
+        uint256 uShareInPool = pool.ShareInPool(); //pool.ShareInPool(msg.sender);
         require(uShareInPool > 0, 'Claim: user has no share in pool');
         // uint256 feeReward = ((((shareInPool[user] / (TOTAL_POOL_SIZE)) *
         //     (feePerShareRate)) / (ACC_TOKEN_PRECISION)) * (FEES_PERCENTAGE)) /
@@ -43,12 +49,12 @@ contract FeeDistribution is Ownable, ReentrancyGuard {
             feePerShareRate *
             FEES_PERCENTAGE *
             ACC_TOKEN_PRECISION) / 100); //(shareInPool[user] *
-        userFeesPerTokenPaid[msg.sender] += feeReward; // need to check what to do with those two params, as we need to subtract the preiously paid 
+        userFeesPerTokenPaid[msg.sender] += feeReward; // need to check what to do with those two params, as we need to subtract the preiously paid
         //fees[msg.sender] += feeReward;                // and keep track of not over paying
         require(
-            feesToken.balanceOf(msg.sender) >= feeReward,
+            feesToken.balanceOf(coverMgmtAddress) >= feeReward,
             'Account: Insufficient balance'
         );
-        feesToken.safeTransfer(msg.sender, feeReward); // for now fees are in this contract
+        feesToken.safeTransferFrom(coverMgmtAddress, msg.sender, feeReward);
     }
 }
